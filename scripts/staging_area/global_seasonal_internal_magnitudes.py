a0df7b6f18f9985763785e92c725a0f4ae5cc645
bradyrx <riley.brady@colorado.edu>
2017-09-22 15:09:04 -0600

    new notebooks

--- scripts/staging_area/global_seasonal_internal_magnitudes.py
+++ scripts/staging_area/global_seasonal_internal_magnitudes.py
@@ -26,6 +26,9 @@ def load_ensemble(VAR):
         ds.attrs = {}
         ds = ds[VAR].to_dataset()
         ds['time'] = pd.date_range('1920-01', '2101-01', freq='M')
+        if (VAR == "FG_CO2") or (VAR == "FG_ALT_CO2"):
+            # Convert to intelligible units.
+            ds[VAR] = ds[VAR] * ((-1 * 3600 * 24 * 365.25) / (1000 * 100))
         return ds
 
 def main():
@@ -36,9 +39,10 @@ def main():
     # Find ensemble mean
     ds = ds.mean(dim='ensemble')
     # Save out and load back in
-    ds.to_netcdf('/glade/scratch/rbrady/temp.nc')
+    temp_file = '/glade/scratch/rbrady/temp_' + VAR + '.nc'
+    ds.to_netcdf(temp_file)
     del ds
-    ds = xr.open_dataset('/glade/scratch/rbrady/temp.nc')
+    ds = xr.open_dataset(temp_file)
     print("Computing seasonal magnitude across grid...")
     ds = ds[VAR].stack(allpoints=['nlat','nlon']) \
                     .groupby('allpoints', squeeze=False) \
@@ -46,21 +50,39 @@ def main():
                     .unstack('allpoints')
     ds.name = 's_magnitude'
     ds.attrs['long name'] = 'Magnitude of 1920-2015 seasonal cycle'
-    ds.to_dataset().to_netcdf('/glade/scratch/rbrady/EBUS_BGC_Variability/' +
-                              VAR + '_seasonal_magnitude_global.nc')
+    # Save as temporary netCDF for memory concerns.
+    seasonal_file = '/glade/scratch/rbrady/seasonal_' + VAR + '.nc'
+    ds.to_dataset().to_netcdf(seasonal_file)
     # To save memory...
     del ds
-    
+    os.remove(temp_file)
     # Find residuals
+    print("Computing residuals across grid...")
     ds = load_ensemble(VAR)
     ds = ds.sel(time=slice('1920-01', '2015-12'))
     ds = ds - ds.mean(dim='ensemble')
     ds = ds[VAR].std(dim='time').mean(dim='ensemble')
     ds.name = 'r_magnitude'
     ds.attrs['long name'] = 'Mean standard deviation of 1920-2015 residuals'
-    ds.to_dataset().to_netcdf('/glade/scratch/rbrady/EBUS_BGC_Variability/' +
-                              VAR + '_internal_magnitude_global.nc')
-    del ds
+    ds = ds.to_dataset()
+    # Load back in seasonal file.
+    ds2 = xr.open_dataset(seasonal_file)
+    os.remove(seasonal_file)
+    variability = xr.Dataset()
+    variability['s_magnitude'] = ds2['s_magnitude']
+    variability['r_magnitude'] = ds['r_magnitude']
+    # Add in global coordinates for reference.
+    coords = xr.open_dataset('/glade/p/work/rbrady/EBUS_BGC_Variability/' + 
+                             'global_coordinates.nc')
+    variability.coords['TLAT'] = coords['TLAT']
+    variability['TAREA'] = coords['TAREA']
+    # Save out!
+    directory = ('/glade/p/work/rbrady/EBUS_BGC_Variability/' + VAR +
+                 '/global/')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    filename = VAR + '_seasonal_internal_global_magnitudes.nc'
+    variability.to_netcdf(directory + filename)
     print("End task.")
 
 if __name__ == '__main__':
--- scripts/staging_area/run_python_script.sh
+++ scripts/staging_area/run_python_script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #PBS -A P93300670
-#PBS -N global_variability
+#PBS -N global_residual_SST
 #PBS -l walltime=10:00:00
 #PBS -M riley.brady@colorado.edu
 #PBS -q economy 
-#PBS -l select=1:ncpus=18
+#PBS -l select=1:ncpus=8
 #PBS -m abe
 
 `source activate py36`
 
-script=global_seasonal_internal_magnitudes.py
-VAR=FG_ALT_CO2
+script=create_global_residuals.py
+VAR=SST
+DIR=/glade/p/work/rbrady/EBUS_BGC_Variability/${VAR}/global/
 
-python ${script} ${VAR}
+python ${script} ${VAR} ${DIR}
 
 
